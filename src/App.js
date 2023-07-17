0ad10e42f74f5d47da8e046f63f34df7319fe91e
Yousef Wael <yousef.wael2002@gmail.com>
2023-07-17 22:38:22 +0300

    Random Quote Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+
+  const [quotes, setQuotes] = useState('');
+  
+
+  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+  
+  const change = () => {
+    const randomColor = colors[Math.floor(Math.random()* colors.length)]
+    setBackgroundColor(randomColor)
+    console.log(backgroundColor);
+
+    // Get a new Random Quote from the array
+    fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    })
+  }
+
+  document.body.style.backgroundColor = backgroundColor;
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+      <p id='text' style={{color: backgroundColor}}><FontAwesomeIcon icon={faQuoteLeft} /> {quotes.text}</p>
+      <p id='author' style={{color: backgroundColor}}> - {quotes.author}</p>
+      <div className='right'>
+      <button className='btn btn-primary float-right' onClick={change} id='new-quote' style={{backgroundColor: backgroundColor}}>New quote</button>
+      </div>
+      <a id='tweet-quote' style={{color: backgroundColor}} href='twitter.com/intent/tweet' target='_blank'><FontAwesomeIcon icon={faTwitter}/></a>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
     <App />
   </React.StrictMode>
 );
